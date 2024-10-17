f8a6f25200347f2f9c03ef58fceed06aa2d1bfb3
ArturoGuzman555 <o.arturoguzman.o@gmail.com>
2024-10-17 14:12:44 -0600

    cors

--- kauzo_back/src/main.ts
+++ kauzo_back/src/main.ts
@@ -8,6 +8,12 @@ require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    credentials: true, 
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
