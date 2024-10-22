d3d0029d78c64ed9ca17dcfd8cef76fb74260cca
ArturoGuzman555 <o.arturoguzman.o@gmail.com>
2024-10-21 19:21:09 -0600

    port

--- kazuo_back/src/main.ts
+++ kazuo_back/src/main.ts
@@ -9,7 +9,7 @@ require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', 
+    origin: 'http://localhost:3000', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
     credentials: true, 
     allowedHeaders: 'Content-Type, Accept, Authorization',
