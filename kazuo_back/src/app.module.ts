577d7f0a4d7a8821a3eb0afcd22b2e8ec7588a71
XsporosX@gmail.com <saul_751@hotmail.com>
2024-10-30 02:01:08 -0400

    modificación de producto

--- kazuo_back/src/app.module.ts
+++ kazuo_back/src/app.module.ts
@@ -21,7 +21,7 @@ import { WebsocketModule } from './modules/websocket/websocket.module';
     FileUploadModule,
     WebsocketModule,
 
-    PaymentModule,
+    // PaymentModule,
     SeedsModule,
 
     ConfigModule.forRoot({
