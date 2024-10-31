bac306f3e1970a55816260723f82997718642414
unknown <fmrigueros91@gmail.com>
2024-10-30 22:35:25 -0500

    Actualizando

--- kazuo_back/src/modules/category/categories-mock.ts
+++ kazuo_back/src/modules/category/categories-mock.ts
@@ -1,13 +1,7 @@
 export const categoriesMock: string[] = [
-<<<<<<< HEAD
   'SmathPhones',
   'Laptops',
   'Tablets',
-=======
-  'Smartphones',
-  'Laptops',
-  'tablets',
->>>>>>> e285cb0e56ad1b6ef211f3da7833e61ed039f920
   'Ropa',
   'Zapatos',
   'Perifericos',
