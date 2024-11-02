ecef8a5e2a30ef7c99a43063ab7b47fbde542dfe
ArturoGuzman555 <o.arturoguzman.o@gmail.com>
2024-11-02 11:20:17 -0600

    prueba3

--- src/modules/checkout/checkout.controller.ts
+++ src/modules/checkout/checkout.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { StripeService } from '../stripe/payment.service';
+// import { Controller, Post, Body } from '@nestjs/common';
+// import { StripeService } from '../stripe/payment.service';
 
-@Controller('checkout')
-export class CheckoutController {
-  constructor(private readonly stripeService: StripeService) {}
+// @Controller('checkout')
+// export class CheckoutController {
+//   constructor(private readonly stripeService: StripeService) {}
 
-  @Post()
-  async createCheckoutSession(@Body('priceId') priceId: string) {
-    const url = await this.stripeService.createCheckoutSession(priceId);
-    return { url };
-  }
-}
+//   @Post()
+//   async createCheckoutSession(@Body('priceId') priceId: string) {
+//     const url = await this.stripeService.createCheckoutSession(priceId);
+//     return { url };
+//   }
+// }
