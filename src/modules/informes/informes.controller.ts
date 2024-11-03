2f4de6db8ed856a0f10d66c7f9eb554420271b49
unknown <fmrigueros91@gmail.com>
2024-11-03 17:36:00 -0500

    Actualizando

--- src/modules/informes/informes.controller.ts
+++ src/modules/informes/informes.controller.ts
@@ -9,12 +9,6 @@ export class InformesController {
   async generarInforme(@Body() informe: any) {
     const pdf = await this.informesService.generarPdf(informe);
     await this.informesService.enviarCorreoElectronico(pdf);
-<<<<<<< HEAD:kazuo_back/src/modules/informes/informes.controller.ts
     return { message: 'Informe generado y enviado con éxito' };
   }
 }
-=======
-    return { message: 'Informe generado y enviado con éxito' };
-  }
-}
->>>>>>> 97a57f209f5fdbf5459fec1fe35715bac56cdb4c:src/modules/informes/informes.controller.ts
