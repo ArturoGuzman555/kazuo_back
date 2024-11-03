0e0c907cf7725796776b9f040367261834a080cd
XsporosX@gmail.com <saul_751@hotmail.com>
2024-11-03 17:04:37 -0400

    intento

--- src/modules/stripe/payment.module.ts
+++ src/modules/stripe/payment.module.ts
@@ -1,15 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { PricesController } from './payment.controller';
-// import { StripeService } from './payment.service';
-
-// @Module({
-//   controllers: [PricesController],
-//   providers: [StripeService],
-// })
-// export class PaymentModule {}
-
-
-// stripe.module.ts
 import { Module } from '@nestjs/common';
 import { StripeController } from './payment.controller';
 import { StripeService } from './payment.service';
