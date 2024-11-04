b9a25acb1e85fe45b49bbca13d5cc05b7bc41db9
XsporosX@gmail.com <saul_751@hotmail.com>
2024-11-04 16:03:26 -0400

    pasarela de pagos

--- src/modules/auth/auth0.strategy.ts
+++ src/modules/auth/auth0.strategy.ts
@@ -1,21 +1,42 @@
-// import { Injectable } from '@nestjs/common';
+// // import { Injectable } from '@nestjs/common';
+// // import { PassportStrategy } from '@nestjs/passport';
+// // import { Strategy } from 'passport-auth0';
+// // import { ConfigService } from '@nestjs/config';
+
+// import { ExtractJwt, Strategy } from 'passport-jwt';
 // import { PassportStrategy } from '@nestjs/passport';
-// import { Strategy } from 'passport-auth0';
+// import { Injectable } from '@nestjs/common';
 // import { ConfigService } from '@nestjs/config';
+// import { Request } from 'express';
 
 // @Injectable()
 // export class Auth0Strategy extends PassportStrategy(Strategy, 'auth0') {
+//   // constructor(private readonly configService: ConfigService) {
+//   //   super({
+//   //     domain: configService.get('AUTH0_DOMAIN'),
+//   //     clientID: configService.get('AUTH0_CLIENT_ID'),
+//   //     clientSecret: configService.get('AUTH0_CLIENT_SECRET'),
+//   //     callbackURL: configService.get('AUTH0_CALLBACK_URL'),
+//   //     scope: 'openid profile email',
+//   //   });
+//   // }
+
 //   constructor(private readonly configService: ConfigService) {
 //     super({
-//       domain: configService.get('AUTH0_DOMAIN'),
-//       clientID: configService.get('AUTH0_CLIENT_ID'),
-//       clientSecret: configService.get('AUTH0_CLIENT_SECRET'),
-//       callbackURL: configService.get('AUTH0_CALLBACK_URL'),
-//       scope: 'openid profile email',
+//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//       ignoreExpiration: false,
+//       secretOrKey: configService.get('JWT_SECRET'),
 //     });
 //   }
 
-//   async validate(accessToken, refreshToken, extraParams, profile) {
-//     return profile;
+//   // async validate(accessToken, refreshToken, extraParams, profile) {
+//   //   return profile;
+//   // }
+
+//   async validate(request: Request, payload: any) {
+//     const roles = payload['https://example.com/roles']; // Obtén los roles del usuario
+//     // Asigna los roles a la solicitud
+//     request['user'] = { ...payload, roles };
+//     return true;
 //   }
 // }
